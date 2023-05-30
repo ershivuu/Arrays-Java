ca18bd44df445bca74be0de0dc10e9119feea93b
Shivam Gupta <125425181+ershivuu@users.noreply.github.com>
2023-05-30 10:35:06 +0530

    Update and rename Main.java to EquilityChecker.java

--- EquilityChecker.java
+++ EquilityChecker.java
@@ -1,17 +1,19 @@
+/*Check whether the two given arrays are equal or not
+i.e., length of the arrays must be same as well as elements of the arrays must be same*/
 public class EquilityChecker {
 
 	public static void main(String[] args) {
-		int A[] = { 1, 2, 5, 4, 0 };
-		int B[] = { 1, 2, 5, 4, 0 };
+		
+		int A[] = { 1, 2, 5, 4, 0 }; // array1
+		int B[] = { 1, 2, 5, 4, 0 }; // array2
 
-		System.out.println(checkArray(A, B));
+		System.out.println(checkArray(A, B)); // calling and printing the checkArray method and its output 
 
 	}
 
 	public static boolean checkArray(int A[], int B[]) {
 		boolean result = true;
 		if (A.length == B.length) {
-
 			for (int i = 0; i < A.length; i++) {
 				if (A[i] != B[i])
 					result = false;
