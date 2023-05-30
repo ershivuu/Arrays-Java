0f82fb690137e6f2edfa864c7af7a6953febdc46
Shivam Gupta <125425181+ershivuu@users.noreply.github.com>
2023-05-30 10:53:27 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+package DuplicateInArray;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int[] arr = { 1, 2, 3, 4, 5, 6, 2 };
+		findDuplicate(arr);
+	}
+
+	public static void findDuplicate(int nums[]) {
+		for (int i = 0; i < nums.length; i++) {
+			for (int j = i + 1; j < nums.length; j++) {
+				if (nums[i] == nums[j]) {
+					System.out.println(nums[j]);
+				}
+			}
+		}
+	}
+
+}
