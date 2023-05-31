afd4e8b55466bdf710fe58de8a8d7f7770df5bca
Shivam Gupta <125425181+ershivuu@users.noreply.github.com>
2023-05-31 08:06:35 +0530

    Update DuplicateInArray.java

--- DuplicateInArray.java
+++ DuplicateInArray.java
@@ -1,11 +1,11 @@
-// find the duplicates in an array using java
+/* find the duplicates in an array and print the duplicate element of an array using java*/
 public class DuplicateInArray {
 
 	public static void main(String[] args) {
 		int[] arr = { 1, 2, 3, 4, 5, 6, 2 };
 		findDuplicate(arr);
 	}
-
+	
 	public static void findDuplicate(int nums[]) {
 		for (int i = 0; i < nums.length; i++) {
 			for (int j = i + 1; j < nums.length; j++) {
